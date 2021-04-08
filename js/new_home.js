08e39f23d2fe3435359a78cfe8e7cf5d190a7c7d
gautamb97 <www.gautam971997@gmail.com>
2021-04-08 16:14:36 +0530

    Refactored:Step2 Delink reference to Employee Id from homeJS

--- js/new_home.js
+++ js/new_home.js
@@ -29,9 +29,9 @@ const createInnerHtml = () => {
                     <td>${empPayrollData._salary}</td>
                     <td>${stringifyDate(empPayrollData._startDate)}</td>
                     <td>
-                        <img name="${empPayrollData._name}" onclick="remove(this)"
+                        <img id="${empPayrollData.id}" onclick="remove(this)"
                             src="../assets/icons/delete-black-18dp.svg" alt="delete">
-                        <img name="${empPayrollData._name}" alt="edit" onclick="update(this)"
+                        <img id="${empPayrollData.id}" alt="edit" onclick="update(this)"
                             src="../assets/icons/create-black-18dp.svg">
                 </td>
             </tr>
@@ -49,11 +49,11 @@ const getDeptHtml = (deptList) => {
 }
 
 const remove = (node) => {
-    let empPayrollData = empPayrollList.find(empData => empData._name == node.name);
+    let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
     if(!empPayrollData) return;
     const index = empPayrollList
-                    .map(empData => empData._name)
-                    .indexOf(empPayrollData._name);
+                    .map(empData => empData.id)
+                    .indexOf(empPayrollData.id);
     empPayrollList.splice(index, 1);
     localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
     document.querySelector(".emp-count").textContent = empPayrollList.length;
